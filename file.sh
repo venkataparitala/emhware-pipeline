88078f0df99e4d64a5763491c5e1a00ee1da00a8
venkataparitala <148308108+venkataparitala@users.noreply.github.com>
2023-10-18 22:29:27 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -4,7 +4,8 @@ filename=$1
 input_string=$2
 
 sudo mkdir -p /files
+sudo ls -lart /
 sudo ls -lart /files/
-sudo echo $input_string > /files/$filename
-
+sudo echo "$input_string" > /files/$filename
+sudo ls -lart
 sudo cat /files/$filename
